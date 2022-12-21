54e8bdd7a15df97e3aeaa14b1aca3363068891f0
Marco Grassi <marco.gra@gmail.com>
2022-12-21 12:28:32 +0800

    upgrade clap to 4, rename parameters to lowercase v and p so we can use the autogenerated implementation of --version and -V to show the tool version

--- src/bin/fusee-gelee.rs
+++ src/bin/fusee-gelee.rs
@@ -1,38 +1,38 @@
-use clap::{crate_version, App, Arg};
+use clap::{crate_version, Arg, Command};
 use std::fs::File;
 
 use fusee::{ExploitDriver, LinuxBackend};
 
 fn main() {
-    let args = App::new("fusee")
+    let args = Command::new("fusee")
         .version(crate_version!())
         .arg(
-            Arg::with_name("payload")
+            Arg::new("payload")
                 .required(true)
-                .takes_value(true)
                 .help("Path to ARM payload to launch"),
         )
         .arg(
-            Arg::with_name("vendor-id")
-                .short("V")
+            Arg::new("vendor-id")
+                .short('v')
                 .default_value("0955")
                 .help("Vendor ID, hexadecimal encoded"),
         )
         .arg(
-            Arg::with_name("product-id")
-                .short("P")
+            Arg::new("product-id")
+                .short('p')
                 .default_value("7321")
                 .help("Product ID, hexadecimal encoded"),
         )
         .get_matches();
 
     let vendor_id = u16::from_str_radix(
-        args.value_of("vendor-id").expect("Vendor ID not provided"),
+        args.get_one::<String>("vendor-id")
+            .expect("Vendor ID not provided"),
         16,
     )
     .expect("Vendor ID must be a 16-bit hexadecimal integer.");
     let product_id = u16::from_str_radix(
-        args.value_of("product-id")
+        args.get_one::<String>("product-id")
             .expect("Product ID not provided"),
         16,
     )
@@ -41,8 +41,11 @@ fn main() {
     let mut driver: ExploitDriver<LinuxBackend> =
         ExploitDriver::discover(vendor_id, product_id).expect("Failed to find USB device");
 
-    let target_payload = File::open(args.value_of("payload").expect("Payload path not provided"))
-        .expect("Failed to open payload file");
+    let target_payload = File::open(
+        args.get_one::<String>("payload")
+            .expect("Payload path not provided"),
+    )
+    .expect("Failed to open payload file");
     driver
         .exploit(target_payload, &fusee::payload::INTERMEZZO_DEFAULT[..])
         .expect("Failed to execute exploit");
